c7517d606040eade633df8708d2c561c2ef82598
Suryakant <surajdwivedi644@gmail.com>
2025-03-12 11:03:22 +0530

    learning new

---
+++ new.js
@@ -0,0 +1,41 @@
+// function f1(val) {
+//     setTimeout(val,3*1000)
+// }
+
+// f1(function () {
+//     console.log(2)
+// })
+
+// var arr = [1,2,3,4,5]
+
+// let a = arr.map((item) =>  item + 5)
+
+// console.log(a)
+
+// function hap (brr,fun){
+//     let newarr = [];
+//     for(let i = 0; i < brr.length; i++){
+//         newarr.push(fun(brr[i]))
+//     }
+//     return newarr;
+// }
+
+// let x = hap(arr,function(val){ return val + 5})
+// console.log(x)
+
+// fun uses clouser to create a counter
+//when we pass a function as args -> callback function
+//when we receive function as parameter or return a function -> hofs
+//when we use the parent function variable in the returning function ->clousers
+
+//  function abc(fun){
+//      fun++;
+//      return add(fun) {console.log(fun)};
+//  }
+ 
+//  abc(function(12))
+
+
+
+
+
